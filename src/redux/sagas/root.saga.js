ea2b4eaab7e15555add26013531cd005bb812a9a
Zoltan Csenyi <cszoltan422@gmail.com>
2021-03-27 11:19:42 +0100

    feature/LMDM-16 - add dashboard.saga.js

--- src/redux/sagas/root.saga.js
+++ src/redux/sagas/root.saga.js
@@ -1,10 +1,10 @@
 import { all } from 'redux-saga/effects';
 import { applicationMountedWatcherSaga } from './application/application.saga';
-import { homepageMountedWatcherSaga } from './homepage/homepage.saga';
+import { dashboardFetchActionWatcherSaga } from './dashboard/dashboard.saga';
 
 export default function* rootSaga() {
     yield all([
         applicationMountedWatcherSaga(),
-        homepageMountedWatcherSaga()
+        dashboardFetchActionWatcherSaga()
     ]);
 }
