93e3812049742c0a1bbac97c065db28b5a82dad6
Zoltan Csenyi <cszoltan422@gmail.com>
2021-08-24 11:35:20 +0200

    LMDM-19 - add location.utils.js and use it in dashboard.saga.js

---
+++ src/util/location/location.utils.js
@@ -0,0 +1,18 @@
+import ENV from '../../environmnent.config';
+import * as Location from 'expo-location';
+import {E2E_MOCK_LOCATION} from "../../application.constants";
+
+export const getLocation = async (accuracy) => {
+    if (ENV.GET_DEVICE_LOCATION) {
+        return Location.getCurrentPositionAsync({
+            accuracy: accuracy
+        });
+    } else {
+        return {
+            coords: {
+                longitude: E2E_MOCK_LOCATION.longitude,
+                latitude: E2E_MOCK_LOCATION.latitude
+            }
+        }
+    }
+}
