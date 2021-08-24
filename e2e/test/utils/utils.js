e5bbf71eb3852f88ecee64c14073849e3a862fbf
Zoltan Csenyi <cszoltan422@gmail.com>
2021-08-24 11:36:18 +0200

    LMDM-19 - log idb stdout and stderr to console

--- e2e/test/utils/utils.js
+++ e2e/test/utils/utils.js
@@ -3,4 +3,5 @@ import { exec } from 'child_process';
 export const setLocation = async (latitude, longitude, device) => {
     const { stdout, stderr } = await exec("idb list-targets | grep -n 'Booted'");
     exec(`idb set-location --udid ${device._deviceId} ${latitude} ${longitude}`);
+    console.log('setLocation - ', stdout, stderr);
 };
