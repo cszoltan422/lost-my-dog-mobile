15b9b959b2e28dd8e9c4eb2d7a414a88a9999dd8
Zoltan Csenyi <cszoltan422@gmail.com>
2021-08-26 00:30:08 +0200

    feature/LMDM-4 - add signup.reducer.js and action.creators.js with action.types.js

--- src/redux/reducers/root.reducer.js
+++ src/redux/reducers/root.reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as appReducer } from './application/application.reducer';
 import { reducer as dashboardReducer } from './dashboard/dashboard.reducer';
 import { reducer as loginReducer } from './login/login.reducer';
+import { reducer as signUpReducer } from './signup/signup.reducer';
 
 const rootReducer = combineReducers({
     application: appReducer,
     dashboard: dashboardReducer,
-    login: loginReducer
+    login: loginReducer,
+    signup: signUpReducer
 });
 
 export default rootReducer;
